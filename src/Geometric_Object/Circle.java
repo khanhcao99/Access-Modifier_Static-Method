7caf86ec9535863ad2369e604caa9e261eb2db80
khanhcao99 <khanhboy1324@gmail.com>
2022-06-08 15:44:45 +0700

    Geometric_Object

---
+++ src/Geometric_Object/Circle.java
@@ -0,0 +1,33 @@
+package Geometric_Object;
+
+public class Circle extends Shape {
+    private double radius = 1.0;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return Math.pow(radius, 2) * Math.PI;
+    }
+
+    public double getPerimeter() {
+        return (radius + radius) * Math.PI;
+    }
+
+    @Override
+    public String toString() {
+        return "A Circle with radius="
+                + getRadius()
+                + ", which is a subclass of "
+                + super.toString();
+    }
+}
